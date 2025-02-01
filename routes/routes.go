c057bb3843e396a5e350db130f4ddbe3464822df
Chanter327 <march09yuuto@gmail.com>
2025-01-31 18:22:06 -0800

    routerパッケージ化

---
+++ routes/routes.go
@@ -0,0 +1,29 @@
+package routes
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func Router() *gin.Engine {
+	r := gin.Default()
+	r.Use(gin.Logger())
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"}, // フロントエンドのURLを指定
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+	r.GET("/api/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello from Butler",
+		})
+	})
+
+	return r
+}
